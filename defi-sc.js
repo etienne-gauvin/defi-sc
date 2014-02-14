dfb35c7de1e55efa5bda567952987624e24b3376
Etienne <etiennegauvin41@gmail.com>
2014-02-14 20:10:58 +0100

    Ajout de l'affichage de l'historique

--- defi-sc.js
+++ defi-sc.js
@@ -1,10 +1,13 @@
 
 // Historique des recherches
-SearchHistory = new Meteor.Collection("search_history");
+SearchHistory = new Meteor.Collection("history");
 
 // Client
 if (Meteor.isClient)
 {
+  Template.history.searchList = function() {
+    return SearchHistory.find({}, {sort: {date: -1}});
+  };
 }
 
 // Serveur
@@ -12,7 +15,8 @@ if (Meteor.isServer)
 {
   Meteor.startup(function ()
   {
-    // Insertion d'une valeur d'exemple
-    SearchHistory.insert({keywords: "#foobar", date: new Date});
+    // Insertion d'une valeur d'exemple si l'historique est vide
+    if (SearchHistory.find().count() === 0)
+      SearchHistory.insert({keywords: "#foobar", date: new Date});
   });
 }
