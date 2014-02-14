5b28bf09ec01e7b2ba9047d2ae73f277ff33f769
Etienne <etiennegauvin41@gmail.com>
2014-02-14 19:42:08 +0100

    Suppression du code inutile créé automatiquement, et initialisation de la base de données de l'historique des recherches

--- defi-sc.js
+++ defi-sc.js
@@ -1,19 +1,18 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to defi-sc.";
-  };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+// Historique des recherches
+SearchHistory = new Meteor.Collection("search_history");
+
+// Client
+if (Meteor.isClient)
+{
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+// Serveur
+if (Meteor.isServer)
+{
+  Meteor.startup(function ()
+  {
+    // Insertion d'une valeur d'exemple
+    SearchHistory.insert({keywords: "#foobar", date: new Date});
   });
 }
