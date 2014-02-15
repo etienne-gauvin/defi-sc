ec3f376fa2485aa1e47c3bcd2cdaa9126bc96d82
Etienne <etiennegauvin41@gmail.com>
2014-02-15 15:12:05 +0100

    Enregistrement des recherches dans l'historique

--- defi-sc.js
+++ defi-sc.js
@@ -1,6 +1,3 @@
-// Client pour l'API Twitter
-var Twit = Meteor.require('twit');
-
 // Historique des recherches
 SearchHistory = new Meteor.Collection("history");
 
@@ -10,11 +7,29 @@ if (Meteor.isClient)
   Template.history.searchList = function() {
     return SearchHistory.find({}, {sort: {date: -1}});
   };
+  
+  Template.search.events({
+    'submit .search-form': function(event)
+    {
+      // Insertion de la nouvelle recherche dans l'historique
+      SearchHistory.insert({
+        keywords: $('.search-form input[type="search"]').val(),
+        date: new Date,
+        user: Meteor.user()._id
+      });
+      
+      // Éviter le rechargement de la page
+      event.preventDefault();
+    }
+  });
 }
 
 // Serveur
 if (Meteor.isServer)
 {
+  // Client pour l'API Twitter
+  var Twit = Meteor.require('twit');
+
   Meteor.startup(function ()
   {
     // Insertion d'une valeur d'exemple si l'historique est vide
